ffd5b0c03e076457d8372cf24bc1088f6fdb5a3a
Ian Fraser <ian.fraser858@gmail.com>
2018-06-19 03:37:22 +0000

    added mongo db

--- app.js
+++ app.js
@@ -1,46 +1,79 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "blah", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df7c57ba5efb7_340.jpg"},
-    {name: "bleh", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104497f8c67da5efb7ba_340.jpg"},
-    {name: "mehh", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c67da5efb7ba_340.jpg"},
-    {name: "blkh", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df7c57ba5efb7_340.jpg"},
-    {name: "bleh", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104497f8c67da5efb7ba_340.jpg"},
-    {name: "mehh", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c67da5efb7ba_340.jpg"},
-    {name: "blah", image: "https://pixabay.com/get/ec31b90f2af61c22d2524518b7444795ea76e5d004b014439df7c57ba5efb7_340.jpg"},
-    {name: "bleh", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104497f8c67da5efb7ba_340.jpg"},
-    {name: "mehh", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104497f8c67da5efb7ba_340.jpg"}
-    ];
-    
-// testing git push
-        
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
 
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+Campground.create(
+    {
+    name: "First Campground",
+    image: "https://cdn.pixabay.com/photo/2017/09/26/13/50/rv-2788677__340.jpg",
+    description: "This is the first campground. The best campground in fact"
+    },
+    function(err, campground) {
+        if(err){
+            console.log(err);
+        }
+        else {
+            console.log(campground);
+        }
+    }    
+);
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds:campgrounds});
-    
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.render("index", {campgrounds:allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res){
-    var campName = req.body.name;
-    var campImage = req.body.image;
-    var newCampground = {campName:campName, campImage:campImage};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-    
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name:name, image:image, description:desc};
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.render("show", {campground:foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
